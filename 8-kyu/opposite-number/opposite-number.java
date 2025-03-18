f1fe5f11d3ccc8ef128246826273990a5d9dc34f
Michal Kucharski <michal.kucharski33@gmail.com>
2025-03-17 23:47:17 -0300

    Add solution - CodeHub

---
+++ 8-kyu/opposite-number/opposite-number.java
@@ -0,0 +1,10 @@
+public class Kata
+    {
+        public static int opposite(int number)
+        {
+          
+            // your code here
+          
+            return number * -1;
+        }
+    }

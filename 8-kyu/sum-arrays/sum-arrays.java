ba4e3e8e609443fbe034e4a07562893f2f61b4e8
Michal Kucharski <michal.kucharski33@gmail.com>
2025-05-30 18:45:14 -0400

    Add solution - CodeHub

---
+++ 8-kyu/sum-arrays/sum-arrays.java
@@ -0,0 +1,14 @@
+public class SumArray {
+​
+  public static double sum(double[] numbers) {
+    double sum = 0.0;
+    if (numbers.length == 0) {
+      return 0;
+    } else {
+      for (int i = 0; i < numbers.length; i++ ) {
+        sum += numbers[i];
+      }
+    }
+    return sum;
+  }
+}

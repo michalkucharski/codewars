76ff76dbfed621fef53f2ee65d8eed5d84ae5551
Michal Kucharski <michal.kucharski33@gmail.com>
2025-05-30 20:58:06 -0400

    Add solution - CodeHub

---
+++ 7-kyu/more-than-zero/more-than-zero.java
@@ -0,0 +1,8 @@
+public  class MoreThanZero {
+  public static String corrections(int x) {
+    if(x > 0)
+      return x + " is more than zero.";
+    else
+      return x + " is equal to or less than zero.";
+  }
+}
